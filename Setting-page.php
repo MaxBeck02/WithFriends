94ee54db3a01b5fb2f443344cb8af9fd4a47a585
Ruurd <74601593+Lux42069@users.noreply.github.com>
2022-03-31 15:25:13 +0200

    Added start php + css

---
+++ Setting-page.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/Stylesheet.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="box1">
+        <break>
+        <h3>Email adress</h3>
+            <p>Change email address</p>
+
+        <h3>Phone number</h3>
+            <p>Change phone number</p>
+
+        <h3>Password</h3>
+            <p>Change password</p>
+
+        <h4>Enable two-factor authentication</h4>
+        <h4>Change user profile picture</h4>
+
+        <div id="buttons">
+            <button> Disable account</button>
+            <button> Delete Account</button>
+        </div>
+
+</div> 
+</body>
+</html>
