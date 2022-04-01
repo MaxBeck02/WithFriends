35f4a6f1b4839de842ec439111b73c2a92e1bbde
MarlydeHaard <61316126+MarlydeHaard@users.noreply.github.com>
2022-04-01 12:26:48 +0200

    Backend connected to server database

--- backend/DbConfig.php
+++ backend/DbConfig.php
@@ -4,7 +4,7 @@ class DbConfig{
 
     public function connect(){
         try{
-            $conn = new PDO("mysql:host=localhost;dbname=withFriends", 'root', '');
+            $conn = new PDO("mysql:host=38.242.233.110;port=3306;dbname=wFriends", 'marly', 'LeidenPlymouth1');
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }catch(PDOException $e){
