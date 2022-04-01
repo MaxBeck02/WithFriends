e98f538571033f6ba4659dc87d15afa90ea88ea6
Ruurd <74601593+Lux42069@users.noreply.github.com>
2022-04-01 14:34:47 +0200

    added/fixed flex pfp

--- Setting-page.php
+++ Setting-page.php
@@ -5,28 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./CSS/Stylesheet.css">
-    <title>Document</title>
+    <title>Setting page</title>
 </head>
 <body>
     <div id="box1">
+        <div id="dubb">
         <break>
 
         <h3>Email adress</h3>
             <input name="Change email address" placeholder="Change email address" maxlength="100" size="30">
+            <input type="submit" value="Submit">
 
         <h3>Phone number</h3>
             <input name="Change phone number" placeholder="Change phone number" maxlength="100" size="30">
+            <input type="submit" value="Submit"> <!--No submit location and no connection-->
 
         <h3>Password</h3>
             <input name="Change password" placeholder="Change password" maxlength="100" size="30">
+            <input type="submit" value="Submit"> <!--No submit location and no connection-->
 
-        <h5>Enable two-factor authentication</h5>
         <h5>Change user profile picture</h5>
 
         <div id="buttons">
-            <button> Disable account</button>
-            <button> Delete Account</button>
+            <button>Disable account</button>
+            <!-- code for acc disable-->
+            <button>Delete Account</button>
+            <!--code for acc delete -->
         </div>
-</div> 
+        </div>
+            <div id="dubb">
+                <a href="#"> <!--add href to file exploder-->
+                    <img src='./img/default-avatar.png' id="pfp" onmouseover="this.src='./img/ChangePFP.png';" onmouseout="this.src='./img/default-avatar.png';" />
+                </a>
+                <button>Change name</button> <!--No submit location and no connection-->
+            </div>  
+    </div> 
+
 </body>
 </html>
