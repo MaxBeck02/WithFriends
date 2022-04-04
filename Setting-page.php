788209e9eb3b020633465ee635be4ba3b2467d04
Ruurd <74601593+Lux42069@users.noreply.github.com>
2022-04-04 14:19:56 +0200

    changed path localhost

--- Setting-page.php
+++ Setting-page.php
@@ -1,10 +1,18 @@
+<?php
+    require_once './partials/footer.html';
+    require_once './partials/header.html';
+    require_once './classes/DbConfig.php'; 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/Stylesheet.css">
+    <link rel="stylesheet" href="./CSS/Stylesheet.css">
+    <link rel="stylesheet" href="./CSS/footer.css">
+    <link rel="stylesheet" href="./CSS/header.css">
     <title>Setting page</title>
 </head>
 <body>
@@ -15,16 +23,20 @@
         <h3>Email adress</h3>
             <input name="Change email address" placeholder="Change email address" maxlength="100" size="30">
             <input type="submit" value="Submit">
+            <br></br>
 
         <h3>Phone number</h3>
             <input name="Change phone number" placeholder="Change phone number" maxlength="100" size="30">
             <input type="submit" value="Submit"> <!--No submit location and no connection-->
-
+            <br></br>
+            
         <h3>Password</h3>
             <input name="Change password" placeholder="Change password" maxlength="100" size="30">
             <input type="submit" value="Submit"> <!--No submit location and no connection-->
+            <br></br>
 
         <h5>Change user profile picture</h5>
+        <br></br>
 
         <div id="buttons">
             <button>Disable account</button>
