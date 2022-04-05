76661ed31f59c37274396d8a88983b31223301d6
Marieke van Rijn <74531663+MariekevRijnland@users.noreply.github.com>
2022-04-05 13:56:26 +0200

    Edited style href and file names
    The stylesheets weren't correctly linked and I also changed the footer and header partials to php instead of html

--- partials/header.php
+++ partials/header.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <head>
-    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="./CSS/header.css">
     <title>W/ Friends</title>
 </head>
 <header>
-    <div id="brand"><a href="/"><img src="../images/With_friends_logo.png" alt=""></a></div>
+    <div id="brand"><a href="/"><img src="./images/With_friends_logo.png" alt=""></a></div>
     <nav>
       <ul>
         <li><a href="/products">Messages</a></li>
         <li><a href="/about">Friends</a></li>
         <li><a href="/about">Name</a></li>
-        <img class="circle" src="../images/user.png" alt="">
+        <img class="circle" src="./images/user.png" alt="">
       </ul>
     </nav>
     <div id="hamburger-icon" onclick="toggleMobileMenu(this)">
